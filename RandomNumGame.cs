a85484fdab8130798bfe3ed5eda262d5d33f0dd4
Jamey Geoghagan <Jameygeoghagan@gmail.com>
2020-06-23 17:22:08 -0500

    creating the doc

---
+++ RandomNumGame.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace RandomNumGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool retry = true;
+            Random random = new Random();
+            do
+            {
+                int randomNumber = random.Next(1, 200);
+                
+
+                while (true)
+                {
+                    Console.Write("Guess any number between 1 and 200: ");
+                    int number = Convert.ToInt32(Console.ReadLine());
+
+                    if (number < randomNumber)
+                    {
+                        Console.WriteLine("Too low, try agian!");
+                       
+                    }
+                    else if (number > randomNumber)
+                    {
+                        Console.WriteLine("Too high, try agian!");
+                       
+                    }
+                    else
+                    {
+                        Console.WriteLine("Great JOB YOU WINN!!");
+                        Console.Write("Would you like to try agian? y/n: ");
+                        string answer = Console.ReadLine();
+                        if (answer != "y")
+                        {
+                            retry = false;
+                        }
+                        break;
+                    }
+                }
+            }
+            while (retry);
+        }
+    }
+}
